8f5ff9b7f8cde266e6fcc4b4016893a71aaf0541
CorrelateVisuals <jakobpovel@protonmail.com>
2023-08-06 01:18:29 +0200

    First working version

--- Image_Color_Manipulation_Kernel/kernel.cu
+++ Image_Color_Manipulation_Kernel/kernel.cu
@@ -3,9 +3,27 @@
 
 #include <iostream>
 #include <string>
+#include <cassert>
 
 #include "STB_Image_Load.h"
 #include "STB_Image_Write.h"
+
+struct alignas(16) Pixel {
+    unsigned char r, g, b, a;
+};
+
+__global__ void imageToGrayScale(unsigned char* imageRGBA) {
+    uint32_t x = blockIdx.x * blockDim.x + threadIdx.x;
+    uint32_t y = blockIdx.y * blockDim.y + threadIdx.y;
+    uint32_t id = y * blockDim.x * gridDim.x + x;
+
+    Pixel* pPixel = (Pixel*)&imageRGBA[id * 4];
+    unsigned char pixelValue = (unsigned char)(pPixel->r * 0.2126f + pPixel->g * 0.7152f + pPixel->b * 0.0722f);
+    pPixel->r = pixelValue;
+    pPixel->g = pixelValue;
+    pPixel->b = pixelValue;
+    pPixel->a = 255;
+}
 
 int main(int argc, char** argv) {
 
@@ -28,6 +46,21 @@ int main(int argc, char** argv) {
         return -1;
     }
 
+    std::cout << "Copy data to GPU ... ";
+    unsigned char* pImageDataGPU = nullptr;
+    assert(cudaMalloc(&pImageDataGPU, width * height * 4) == cudaSuccess);
+    assert(cudaMemcpy(pImageDataGPU, imageData, width * height * 4, cudaMemcpyHostToDevice) == cudaSuccess);
+
+    std::cout << "Running CUDA kernel ... ";
+    dim3 blockSize(32, 32, 1);
+    dim3 gridSize(width / blockSize.x, height / blockSize.y);
+    imageToGrayScale<<<gridSize, blockSize>>>(pImageDataGPU);
+    std::cout << "DONE" << std::endl;
+
+    std::cout << "Copy data from GPU ... ";
+    assert(cudaMemcpy(imageData, pImageDataGPU, width * height * 4, cudaMemcpyDeviceToHost) == cudaSuccess);
+    std::cout << "DONE" << std::endl;
+
     std::string fileNameOut = argv[1];
     int strideBytes = sizeof(int) * width;
 
@@ -37,6 +70,7 @@ int main(int argc, char** argv) {
     stbi_write_png(newFileName.c_str(), width, height, channels, imageData, strideBytes);
     std::cout << "Done " << fileNameOut << " saved" << std::endl;
 
+    cudaFree(pImageDataGPU);
     stbi_image_free(imageData);
 
     return 0;
