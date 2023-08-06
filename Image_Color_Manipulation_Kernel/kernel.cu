956b3988807b1e5faf58bc6141d5897621ae73c9
CorrelateVisuals <jakobpovel@protonmail.com>
2023-08-06 15:36:06 +0200

    Added global variables

--- Image_Color_Manipulation_Kernel/kernel.cu
+++ Image_Color_Manipulation_Kernel/kernel.cu
@@ -8,21 +8,31 @@
 #include "STB_Image_Load.h"
 #include "STB_Image_Write.h"
 
-struct alignas(16) Pixel {
+const struct alignas(16) Pixel {
     unsigned char r, g, b, a;
 };
+const int pixelSizeBytes = sizeof(Pixel);
+const int channels = pixelSizeBytes / sizeof(int);
+const unsigned char maxPixelIntensity = 255;
 
-__global__ void imageToGrayScale(unsigned char* imageRGBA) {
+struct greyScaleRGB {
+    float r = 0.2126f;
+    float g = 0.7152f;
+    float b = 0.0722f;
+};
+
+__global__ void imageToGreyScale(unsigned char* imageRGBA) {
     uint32_t x = blockIdx.x * blockDim.x + threadIdx.x;
     uint32_t y = blockIdx.y * blockDim.y + threadIdx.y;
     uint32_t id = y * blockDim.x * gridDim.x + x;
 
-    Pixel* pPixel = (Pixel*)&imageRGBA[id * sizeof(int)];
-    unsigned char pixelValue = (unsigned char)(pPixel->r * 0.2126f + pPixel->g * 0.7152f + pPixel->b * 0.0722f);
+    Pixel* pPixel = (Pixel*)&imageRGBA[id * channels];
+    greyScaleRGB weight;
+    unsigned char pixelValue = (unsigned char)(pPixel->r * weight.r + pPixel->g * weight.g + pPixel->b * weight.b);
     pPixel->r = pixelValue;
     pPixel->g = pixelValue;
     pPixel->b = pixelValue;
-    pPixel->a = 255;
+    pPixel->a = maxPixelIntensity;
 }
 
 int main(int argc, char** argv) {
@@ -32,7 +42,7 @@ int main(int argc, char** argv) {
         return -1;
     }
 
-    int width, height, componentCount, channels = sizeof(Pixel) / sizeof(int);
+    int width, height, componentCount;
     std::cout << "Loading png file ... ";
     unsigned char* imageData = stbi_load(argv[1], &width, &height, &componentCount, channels);
     if (!imageData) {
@@ -48,25 +58,26 @@ int main(int argc, char** argv) {
 
     std::cout << "Copy data to GPU ... ";
     unsigned char* pImageDataGPU = nullptr;
-    assert(cudaMalloc(&pImageDataGPU, width * height * sizeof(int)) == cudaSuccess);
-    assert(cudaMemcpy(pImageDataGPU, imageData, width * height * sizeof(int), cudaMemcpyHostToDevice) == cudaSuccess);
+    assert(cudaMalloc(&pImageDataGPU, width * height * channels) == cudaSuccess);
+    assert(cudaMemcpy(pImageDataGPU, imageData, width * height * channels, cudaMemcpyHostToDevice) == cudaSuccess);
 
     std::cout << "Running CUDA kernel ... ";
-    dim3 blockSize(32, 32, 1);
+    const int blockDimension = 32;
+    dim3 blockSize(blockDimension, blockDimension, 1);
     dim3 gridSize(width / blockSize.x, height / blockSize.y);
-    imageToGrayScale<<<gridSize, blockSize>>>(pImageDataGPU);
+    imageToGreyScale<<<gridSize, blockSize>>>(pImageDataGPU);
     std::cout << "DONE" << std::endl;
 
     std::cout << "Copy data from GPU ... ";
-    assert(cudaMemcpy(imageData, pImageDataGPU, width * height * sizeof(int), cudaMemcpyDeviceToHost) == cudaSuccess);
+    assert(cudaMemcpy(imageData, pImageDataGPU, width * height * channels, cudaMemcpyDeviceToHost) == cudaSuccess);
     std::cout << "DONE" << std::endl;
 
     std::string fileNameOut = argv[1];
-    int strideBytes = sizeof(int) * width;
+    int strideBytes = channels * width;
 
     std::cout << "Writing png to disk ... ";
     std::string baseFileName = fileNameOut.substr(0, fileNameOut.find_last_of("."));
-    std::string newFileName = baseFileName + "_gray.jpg";
+    std::string newFileName = baseFileName + "_grey.jpg";
     stbi_write_png(newFileName.c_str(), width, height, channels, imageData, strideBytes);
     std::cout << "Done " << fileNameOut << " saved" << std::endl;
 
