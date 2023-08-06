8a7c1533942f16f83bce20c170eff36155209603
CorrelateVisuals <jakobpovel@protonmail.com>
2023-08-06 21:52:28 +0200

    Formatting and optimization

--- Image_Color_Manipulation_Kernel/kernel.cu
+++ Image_Color_Manipulation_Kernel/kernel.cu
@@ -8,17 +8,17 @@
 #include "STB_Image_Load.h"
 #include "STB_Image_Write.h"
 
-const struct alignas(16) Pixel {
+struct alignas(16) Pixel {
     unsigned char r, g, b, a;
 };
 const int pixelSizeBytes = sizeof(Pixel);
 const int channels = pixelSizeBytes / sizeof(int);
 const unsigned char maxIntensity = 255;
 
-struct greyScaleRGB {
-    float r = 0.2126f;
-    float g = 0.7152f;
-    float b = 0.0722f;
+const struct alignas(16) greyScaleRGB {
+    const float r = 0.2126f;
+    const float g = 0.7152f;
+    const float b = 0.0722f;
 };
 
 const int blockDimension = 32;
@@ -57,64 +57,47 @@ __global__ void imageToGreyscale(unsigned char* imageRGBA) {
     pPixel->a = maxIntensity;
 }
 
-int main(int argc, char** argv) {
+void copyDataAndWriteToDisk(unsigned char* pImageDataGPU, unsigned char* imageData, int width, int height, int channels, const std::string& fileNameOut, int strideBytes, const std::string& suffix) {
+    std::cout << "Copy data from GPU ... ";
+    assert(cudaMemcpy(imageData, pImageDataGPU, width * height * channels, cudaMemcpyDeviceToHost) == cudaSuccess);
+    std::cout << "DONE" << std::endl;
 
-    if (argc < 2) {
-        std::cout << "Usage: Image Color Manipulation <filename>" << std::endl;
-        return -1;
-    }
+    std::cout << "Writing png to disk ... ";
+    std::string baseFileName = fileNameOut.substr(0, fileNameOut.find_last_of("."));
+    std::string newFileName = baseFileName + suffix;
+    stbi_write_png(newFileName.c_str(), width, height, channels, imageData, strideBytes);
+    std::cout << "Done " << fileNameOut << " saved" << std::endl;
+}
+
+int main(int argc, char** argv) {
+    if (argc < 2) { std::cout << "Usage: Image Color Manipulation <filename>" << std::endl; return -1; }
 
     int width, height, componentCount;
     std::cout << "Loading png file ... ";
     unsigned char* imageData = stbi_load(argv[1], &width, &height, &componentCount, channels);
-    if (!imageData) {
-        std::cout << "Failed to open \"" << argv[1] << "\"";
-        return -1;
-    }
+    if (!imageData) { std::cout << "Failed to open \"" << argv[1] << "\"";  return -1; }
     std::cout << "Done" << std::endl;
 
-    if (width % 32 || height % 32) {
-        std::cout << "Width or height is not devidible by 32; leaked memory of imageData";
-        return -1;
-    }
-
     std::cout << "Copy data to GPU ... ";
+    if (width % 32 || height % 32) { std::cout << "Width or height is not devidible by 32; leaked memory of imageData"; return -1; }
     unsigned char* pImageDataGPU = nullptr;
     assert(cudaMalloc(&pImageDataGPU, width * height * channels) == cudaSuccess);
     assert(cudaMemcpy(pImageDataGPU, imageData, width * height * channels, cudaMemcpyHostToDevice) == cudaSuccess);
 
-    std::cout << "Running CUDA kernel ... ";
     dim3 blockSize(blockDimension, blockDimension, 1);
     dim3 gridSize(width / blockSize.x, height / blockSize.y);
-    imageToGreyscale<<<gridSize, blockSize>>>(pImageDataGPU);
-    std::cout << "DONE" << std::endl;
-
-    std::cout << "Copy data from GPU ... ";
-    assert(cudaMemcpy(imageData, pImageDataGPU, width * height * channels, cudaMemcpyDeviceToHost) == cudaSuccess);
-    std::cout << "DONE" << std::endl;
-
     std::string fileNameOut = argv[1];
     int strideBytes = channels * width;
 
-    std::cout << "Writing png to disk ... ";
-    std::string baseFileName = fileNameOut.substr(0, fileNameOut.find_last_of("."));
-    std::string newFileName = baseFileName + "_grey.jpg";
-    stbi_write_png(newFileName.c_str(), width, height, channels, imageData, strideBytes);
-    std::cout << "Done " << fileNameOut << " saved" << std::endl;
+    std::cout << "Running CUDA kernel ... ";
+    imageToGreyscale<<<gridSize, blockSize>>>(pImageDataGPU);
+    std::cout << "DONE" << std::endl;
+    copyDataAndWriteToDisk(pImageDataGPU, imageData, width, height, channels, fileNameOut, strideBytes, "_grey.jpg");
 
     std::cout << "Running CUDA kernel ... ";
     threadsOnImage<<<gridSize, blockSize>>>(pImageDataGPU);
     std::cout << "DONE" << std::endl;
-
-    std::cout << "Copy data from GPU ... ";
-    assert(cudaMemcpy(imageData, pImageDataGPU, width * height * channels, cudaMemcpyDeviceToHost) == cudaSuccess);
-    std::cout << "DONE" << std::endl;
-
-    std::cout << "Writing png to disk ... ";
-    baseFileName = fileNameOut.substr(0, fileNameOut.find_last_of("."));
-    newFileName = baseFileName + "_threads.jpg";
-    stbi_write_png(newFileName.c_str(), width, height, channels, imageData, strideBytes);
-    std::cout << "Done " << fileNameOut << " saved" << std::endl;
+    copyDataAndWriteToDisk(pImageDataGPU, imageData, width, height, channels, fileNameOut, strideBytes, "_threads.jpg");
 
     cudaFree(pImageDataGPU);
     stbi_image_free(imageData);
